21d05e204c004dbe1eb14fe51d2565b8e45a6644
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-06-14 14:24:08 +0800

    Aletr collation

--- function_mapping.sql
+++ function_mapping.sql
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `user` (
   `id` int NOT NULL,
   `fullname` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
